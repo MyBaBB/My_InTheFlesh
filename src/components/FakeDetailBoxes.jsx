d3342035cdfdf33c30ee3abc8f3c94b92a011120
Brett Baker <GitBaBB@MyBaBB.com>
2024-01-14 10:24:36 -0800

    Ok this make a good example for the script, maybe it will sell ,  thats all i really have to do until they decide to buy or not,  Its a good example and it was a good learning experience. its fake so run it in F11 for full screen, it will trick you it a Movie Fake thing.

--- src/components/FakeDetailBoxes.jsx
+++ src/components/FakeDetailBoxes.jsx
@@ -8,11 +8,176 @@ const FakeDetailBoxes = () => {
 
 
 
-  <div className='FakeRegistrationBody w-full h-full relative justify-evenly items-center flex-row
-   bg-black p-2'>
-     <div className='FakeRegistrationInput relative justify-center items-center
+  <div className='FakeRegistrationBody w-full h-full relative justify-center items-center  flex-col mx-auto
+   bg-black p-2 mt-[9.5rem]'>
+      <div className='FakeRegistrationInput relative justify-center items-center p-2
+    border-transparent m-2 rounded-3xl min-w-[300px] max-w-[600px] h-fit
+    bg-gradient-to-b from-gray-900 via-gray-700 to-gray-900 mx-auto '>
+
+      
+      <h2 className='Likeness text-center pb-2'>Likeness: <span className='UserName'>&nbsp; &quot;Jay&quot;</span></h2>
+      <div className=' relative flex  justify-center '>
+       <div className='likenessButton leading-7 m-2 relative flex justify-center p-4 mt-3 border-4 border-gray-900 rounded-3xl w-[80px] h-fit ' >
+        <span className='likenessButtonSpan relative text-center'>Upload <br /> Images</span>
+       
+
+
+ 
+       
+         
+       </div>
+       <div className='PreviouslyScannedWrapper relative  flex w-full  '>
+       <div className='previouslyScanned relative items-center flex    p-2 
+           ml-[0rem] xxs:ml-[0rem] xs:ml-[1rem] sm:ml-[3rem] md:ml-[4rem] lg:ml-[4rem] xl-[6rem] 
+           text-[10px] xxs:text-[10px] xs:text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px]
+         '> <p className='inline-block   '>Images Previously Scanned</p>
+         </div> 
+        </div>     
+       </div>
+     </div>
+
+
+
+     
+     <div className='FakeRegistrationInput  relative justify-center items-center p-2
+    border-transparent m-2 rounded-3xl min-w-[300px] max-w-[600px] h-fit
+    bg-gradient-to-b from-gray-900 via-gray-700 to-gray-900 mx-auto '>
+<h2 className='Likeness text-center pb-2'>Body Style:</h2>
+<div className='PreviouslyScannedWrapper  relative  flex w-full  '>
+       <div className='previouslyScanned  w-full relative justify-evenly flex p-1 
+           ] 
+           text-[8px] xxs:text-[9px] xs:text-[12px] sm:text-[14px] md:text-[18px] lg:text-[20px]
+         '> 
+        <div className='BodyPartsLeft'>
+          <p className='BodyParts '>Age:&nbsp;29 </p>
+          <p className='BodyParts '>Height:&nbsp;6&apos;</p>
+        </div>
+      <div className='BodyPartsLeft'>
+          <p className='BodyParts'>Weight:&nbsp;255-lbs</p>
+          <p className='BodyParts'>Bust:&nbsp;57&quot;</p>
+          </div>
+      <div className='BodyPartsRight'> 
+          <p className='BodyParts'>Waist:&nbsp;34&quot;</p>
+          <p className='BodyParts'>Hips:&nbsp;N/A</p>
+      </div>      
+      <div className='BodyPartsRight'> 
+          <p className='BodyParts'>Neck:&nbsp;20&quot;</p>
+          <p className='BodyParts'>Shoe:&nbsp;19</p>
+             
+      </div>
+         </div> 
+        </div> 
+
+     </div>
+     <div className='FakeRegistrationInput border-2 border-red-500  relative justify-center items-center p-2
+    border-transparent m-2 rounded-3xl min-w-[300px] max-w-[600px] h-fit
+    bg-gradient-to-b from-gray-900 via-gray-700 to-gray-900 mx-auto '>
+<h2 className='Likeness text-center pb-2'>Personality:</h2>
+
+
+      <div className='PreviouslyScannedWrapper relative  flex w-full  '>
+        <div className='previouslyScanned w-full relative justify-evenly flex p-1 
+            
+           text-[8px] xxs:text-[9px] xs:text-[12px] sm:text-[14px] md:text-[18px] lg:text-[20px]
+             '> 
+              <p className='slider relative flex justify-center items-center
+              '>Reserved</p>
+        <div className=' PersonalitySliders1 border-2 border-gray-900   
+            relative flex justify-evenly w-[50%] h-[14px] rounded-xl my-8' >
+            </div>
+              <p className='slider relative flex justify-center items-center
+              text-blue-300'>Flirtatious</p>
+             </div> 
+         </div> 
+
+
+      <div className='PreviouslyScannedWrapper relative  flex w-full  '>
+        <div className='previouslyScanned w-full relative justify-evenly flex p-1 
+            
+           text-[8px] xxs:text-[9px] xs:text-[12px] sm:text-[14px] md:text-[18px] lg:text-[20px]
+             '> 
+              <p className='slider relative text-center flex justify-center items-center
+              '>Emotional <br /> Closed</p>
+        <div className=' PersonalitySliders2 border-2 border-gray-900   
+            relative flex justify-evenly w-[50%] h-[14px] rounded-xl my-8' >
+            </div>
+              <p className='slider relative flex justify-center text-center items-center
+              text-blue-300'>Emotional <br />  Open</p>
+             </div> 
+         </div> 
+
+
+      <div className='PreviouslyScannedWrapper relative  flex w-full  '>
+        <div className='previouslyScanned w-full relative justify-evenly flex p-1 
+            
+           text-[8px] xxs:text-[9px] xs:text-[12px] sm:text-[14px] md:text-[18px] lg:text-[20px]
+             '> 
+              <p className='slider relative flex justify-center items-center
+              '>Intraverted</p>
+        <div className=' PersonalitySliders3 border-2 border-gray-900   
+            relative flex justify-evenly w-[50%] h-[14px] rounded-xl my-8' >
+            </div>
+              <p className='slider relative flex justify-center items-center
+              text-blue-300'>Extraverted</p>
+             </div> 
+         </div> 
+
+
+      <div className='PreviouslyScannedWrapper relative  flex w-full  '>
+        <div className='previouslyScanned w-full relative justify-evenly flex p-1 
+            
+           text-[8px] xxs:text-[9px] xs:text-[12px] sm:text-[14px] md:text-[18px] lg:text-[20px]
+             '> 
+              <p className='slider relative flex justify-center items-center
+              '>Sensing</p>
+        <div className=' PersonalitySliders4 border-2 border-gray-900   
+            relative flex justify-evenly w-[50%] h-[14px] rounded-xl my-8' >
+            </div>
+              <p className='slider relative flex justify-center items-center
+              text-blue-300'>Intuition</p>
+             </div> 
+         </div> 
+
+     </div>
+
+
+
+
+
+    {/* 
+                        EMPTY BOXES    
+    <div className='FakeRegistrationInput relative justify-center items-center
     border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
 
+     </div>
+ */}
+
+
+
+
+
+{/* 
+
+
+     <div className='FakeRegistrationInput relative justify-center items-center
+    border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
+There are sections for Height, Weight, and standard fitting
+dimensions - Bust, Waist, Hips.
+Everything has already been filled out with precise
+dimensions that are obviously not Jay’s own.
+Jay scrolls down further...
+There are a number PERSONALITY SLIDERS.
+
+One has “RESERVED” on the left side and “FLIRTATIOUS” on the
+right side - the slider is about 80% of the way towards
+“FLIRTATIOUS.”
+One has “EMOTIONALLY CLOSED” on one side and “EMOTIONALLY
+OPEN” on the other, slider is 70% towards “OPEN.”
+“EXTRAVERSION” / “INTROVERSION” - 95% extraverted.
+“SENSING” / “INTUITION” - 75% sensing. More “FEELING” than
+“THINKING.” More “PERCEIVING” than “JUDGING.”
+Almost all the way towards “PASSIVE” versus “AGGRESSIVE.”
+“MENTAL CONNECTION” and “SEXUAL CONNECTION” - 70% towards
      </div>
      <div className='FakeRegistrationInput relative justify-center items-center
     border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
@@ -25,15 +190,7 @@ const FakeDetailBoxes = () => {
      <div className='FakeRegistrationInput relative justify-center items-center
     border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
 
-     </div>
-     <div className='FakeRegistrationInput relative justify-center items-center
-    border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
-
-     </div>
-     <div className='FakeRegistrationInput relative justify-center items-center
-    border-2 border-green-700 rounded-3xl text-green-600 w-[320px] h-[150px] '>
-
-     </div>
+     </div> */}
      </div>
 
 
