c64a3e4b646e6ff045f8f7699cb2d77d7e354265
Brett Baker <GitBaBB@MyBaBB.com>
2024-01-18 20:02:30 -0800

    Update Copyright.jsx
    Added bottom-0 to copywrite

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -5,7 +5,7 @@ function Copyright  () {
   return (
     <div>
 
-<p className="fixed bottom-0 right-5 whitespace-nowrap text-xs">
+<p className="fixed bottom-1 right-5 whitespace-nowrap text-xs">
   <span className="text-varBoneWhite dark:text-varLIGHTWING text-[7px] sm:text-[19px] z-0">
     &lt;&lt;&lt;&copy;MyBaBB <em>1--14-2014</em>&gt;&gt;&gt;
   </span>
